fd96e56a4751db3a9237519ac8a02734045bd2ec
David Nolan <davidtmnolan@gmail.com>
2014-06-27 12:52:31 -0400

    Redirect to URLs, not paths; always redirect to login_url after a failed login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,19 +3,20 @@ class UserSessionsController < ApplicationController
 	end
 	
 	def create
-		user = User.find_by(email: params[:email])
+		user = User.find_by_email(params[:email])
+
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to todo_lists_path
+			redirect_to todo_lists_url
 		else
-			flash[:error] = "Unable to log in.  Please check your email and password."
-			render action: 'new'
+			flash[:error] = "Unable to log in. Please check your email and password."
+			redirect_to login_url
 		end
 	end
 	
 	def destroy
 		session[:user_id] = nil
 		reset_session
-		redirect_to root_path, notice: "Logged out"
+		redirect_to root_url, notice: "Logged out"
 	end
 end
