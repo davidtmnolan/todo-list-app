ba7ac68d813443603295375c1b4be9b05cc0b33b
David Nolan <davidtmnolan@gmail.com>
2014-07-02 23:29:43 -0400

    Make item scopes more readible and add complete class methods

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,14 +1,20 @@
 class TodoItem < ActiveRecord::Base
-	
-	scope :complete, -> { where("completed_at is not null") }
-	scope :incomplete, -> { where(completed_at: nil) }
-	
 	belongs_to :todo_list
-	
+
 	validates :content, presence: true, length: { minimum: 3 }
 	
+	scope :pending,   -> { where('completed_at is null') }
+	scope :completed, -> { where('completed_at is not null') }
+
 	def completed?
-		!completed_at.blank?
+		completed_at?
 	end
-	
+
+  def complete!
+    update_attribute :completed_at, Time.now.utc
+  end
+
+  def uncomplete!
+    update_attribute :completed_at, nil
+  end
 end
