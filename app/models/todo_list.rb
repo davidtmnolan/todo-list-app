cb5f64a3d4871dce1c47e448aa329e510bb8eafc
David Nolan <davidtmnolan@gmail.com>
2014-07-02 23:36:42 -0400

    Revise class methods, remove arbitrary scope

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,24 +1,14 @@
 class TodoList < ActiveRecord::Base
-	
-	scope :newest, -> { order("created_at DESC") }
-	
-	validates :title, presence: true, length: { minimum: 2}
-	has_many :todo_items, dependent: :destroy
 	belongs_to :user
+	has_many :todo_items, dependent: :destroy
 	
-	def empty_list?
-		todo_items.count == 0
-	end
-	
-	def has_completed_items?
-		todo_items.complete.size > 0
-	end
-	
-	def items_completed
-		todo_items.complete.size
-	end
-	
-	def size
-		todo_items.size
-	end
+	validates :title, presence: true, length: { minimum: 2 }
+
+  def empty?
+    todo_items.empty?
+  end
+
+  def completed?
+    todo_items.all?(&:completed?)
+  end
 end
