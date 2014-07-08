3a38091f681e0328740a3b89da92c069433f26d4
David Nolan <davidtmnolan@gmail.com>
2014-07-08 17:47:17 -0400

    Add full_name method, add to layout.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-	has_secure_password
-	
-	has_many :todo_lists, dependent: :destroy
-	
-	validates :email, presence: true, 
-										uniqueness: true,
-										format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/ }
-										
+  has_secure_password
+
+  has_many :todo_lists, dependent: :destroy
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/ }
+
+  def full_name
+    name = first_name + ' ' + last_name
+    name.titleize;
+  end
+
 end
