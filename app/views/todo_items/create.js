b35a328e0dbe100489aa771f7cc47453c7cbe914
David Nolan <davidtmnolan@gmail.com>
2014-07-09 19:38:05 -0400

    Rename todo_item to pending_todo_item

--- app/views/todo_items/create.js
+++ app/views/todo_items/create.js
@@ -1,4 +1,4 @@
 /********** Adds new item to table row *********/
-$('#pending-items tr:first').after('<%= j render 'todo_items/todo_item', todo_list: @todo_list, item: @todo_item %>');
+$('#pending-items tr:first').after('<%= j render 'todo_items/pending_todo_item', todo_list: @todo_list, item: @todo_item %>');
 /********** Clears new item input *********/
 $(':input').val("");
