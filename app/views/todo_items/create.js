252da510a6fc8f488a8d2908730d7e06d142e76a
David Nolan <davidtmnolan@gmail.com>
2014-07-16 20:04:20 -0400

    Change rendered partial to reflect rename

--- app/views/todo_items/create.js
+++ app/views/todo_items/create.js
@@ -1,4 +1,4 @@
 /********** Adds new item to table row *********/
-$('#pending-items tr:first').after('<%= j render 'todo_items/pending_todo_item', todo_list: @todo_list, item: @todo_item %>');
+$('#pending-items tr:first').after('<%= j render 'todo_items/pending', todo_list: @todo_list, item: @todo_item %>').slideDown(200);
 /********** Clears new item input *********/
 $(':input').val("");
